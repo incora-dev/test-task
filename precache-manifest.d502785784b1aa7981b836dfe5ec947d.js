03476586d9bfa935dad02184efa6905d99bdbb4f
Incora <dev@incorainc.com>
2019-02-22 11:54:44 +0200

    Updates

---
+++ precache-manifest.d502785784b1aa7981b836dfe5ec947d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a792770b6ea4478cc049",
+    "url": "/test-task/static/js/runtime~main.a792770b.js"
+  },
+  {
+    "revision": "b23723bef94895e9d468",
+    "url": "/test-task/static/js/main.b23723be.chunk.js"
+  },
+  {
+    "revision": "ba9948b3a45a16f0ab85",
+    "url": "/test-task/static/js/1.ba9948b3.chunk.js"
+  },
+  {
+    "revision": "b23723bef94895e9d468",
+    "url": "/test-task/static/css/main.090a4baa.chunk.css"
+  },
+  {
+    "revision": "a56ba2b0952d8f60fb14215cf48b88fe",
+    "url": "/test-task/index.html"
+  }
+];
